1a1fde1791a02104fe252c9a2f7d235498a8ed66
Pyae Phyo Zan <pyae418283@gmail.com>
2023-04-02 21:18:12 +0630

    add language system route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('locale/{locale}', function ($locale) {
+    Session::put('locale',$locale);
+
+    return redirect()->back();
+});
 
 Route::group(['namespace' => 'App\Http\Controllers'],function () {
     Route::get('/','HomeController@index')->name('home.index');
