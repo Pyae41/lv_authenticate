47d493270e0aec511921b93f1ba7e10b19cec257
Pyae Phyo Zan <pyae418283@gmail.com>
2023-04-02 21:19:15 +0630

    English Language

---
+++ resources/lang/en/langs.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'sign-up' => 'Sign-up',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'home' => 'home',
+    'features' => 'Features',
+    'pricing' => 'Pricing',
+    'about' => 'About',
+    'home-page' => "Home Page",
+    'not_logged_in' => "You are in home page",
+    // Product
+    'products' => 'Products',
+    'create_product' => 'Create New Product',
+    'update_product' => 'Edit Product',
+
+    // table product
+    't_no' => 'No',
+    't_product_name' => 'Product Name',
+    't_product_price' => 'Price',
+    't_product_quantity' => 'Quantity',
+
+    'pro_placeholder_name' => 'Enter Product Name',
+    'pro_placeholder_price' => 'Enter Price',
+    'pro_placeholder_quantity' => 'Enter Quantity',
+
+    // table product
+
+    // button text
+    'edit_button' => 'Edit',
+    'delete_button' => 'Delete',
+    'back_button' => 'Back',
+    'create_button' => 'Create',
+    'update_button' => 'Update',
+    't_action' => 'Actions',
+];
